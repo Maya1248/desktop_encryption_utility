887af2a25d38d2c860372ed1c23e8af8029d5c8a
Maya Weber <mayamaya124816@gmail.com>
2025-05-01 18:20:00 +0200

    Added a bunch of safeguards. Also most/all of the file_ops.h methods are forked from another abandoned project of mine, but are still my code so if you are confused on what the hell its all doing, dm me or something :P

--- lib/file_ops.h
+++ lib/file_ops.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "../lib/AES_256_ECB.h"
+#include "../lib/SHA_256.h"
 
 #endif
 
@@ -19,3 +20,123 @@ int is_writable(char *file_name) {
     return 1;
 }
 
+long long get_file_size(char *relative_name) {
+    FILE *fptr = fopen(relative_name, "rb");
+
+    fseek(fptr, 0, SEEK_END);
+    long long file_size = ftell(fptr);
+    fseek(fptr, 0, SEEK_SET);
+
+    fclose(fptr);
+
+    return file_size;
+}
+
+int hasEnc(char *file_name) {
+    int index = strlen(file_name) - 1;
+    if (file_name[index - 3] == '.' && file_name[index - 2] == 'e' && file_name[index - 1] == 'n' && file_name[index] == 'c') {
+        return 0;
+    }
+
+    return -1;
+}
+
+int check_password(char *password_string, char *file_name) {
+    FILE *fptr = fopen(file_name, "rb+");
+    unsigned char *password_hash = hash(password_string);
+    unsigned char password_hash_buffer[32];
+
+    fread(password_hash_buffer, sizeof(unsigned char), 32, fptr);
+    for (int i = 0; i < 32; i++) {
+        if (password_hash[i] != password_hash_buffer[i]) {
+            free(password_hash);
+            fclose(fptr);
+            return 1;
+        }
+    }
+
+    free(password_hash);
+    fclose(fptr);
+    return 0;
+}
+
+void add_padding_to_file(FILE *fptr, long long file_size) {  // Does NOT close the file.
+    int check = file_size % AES_BLOCK_SIZE;
+    
+    if (check != 0) {
+        fseek(fptr, 0, SEEK_END);
+
+        int amount_to_add = AES_BLOCK_SIZE - check; // check for how many zeros have to be added for it to add up to a multiple of AES_BLOCK_SIZE
+        unsigned char tmp_buffer[AES_BLOCK_SIZE];
+        memset(tmp_buffer, 0, AES_BLOCK_SIZE);
+        fwrite(tmp_buffer, sizeof(unsigned char), amount_to_add, fptr);   // pad the file
+
+        fseek(fptr, 0, SEEK_SET);
+    }
+}
+
+void encrypt_file(char *file_name, unsigned char *key) {
+    FILE *fptr;
+
+    AES_CTX ctx;
+    unsigned char data_buffer[AES_BLOCK_SIZE];  //  bytes storing data; 16 bytes
+
+    long long file_size = get_file_size(file_name);
+    fptr = fopen(file_name, "rb+");
+
+    add_padding_to_file(fptr, file_size);
+    AES_EncryptInit(&ctx, key);
+
+    for (long long i = 0; i < file_size; i += AES_BLOCK_SIZE) {
+        fread(data_buffer, sizeof(unsigned char), AES_BLOCK_SIZE, fptr);
+        fseek(fptr, i, SEEK_SET);
+        AES_Encrypt(&ctx, data_buffer, data_buffer);
+        fwrite(data_buffer, sizeof(unsigned char), AES_BLOCK_SIZE, fptr);
+    }
+    
+    AES_CTX_Free(&ctx);    
+    fclose(fptr);
+
+    char *new_name = (char *)calloc(255, sizeof(char));
+    strcat(new_name, file_name);
+    strcat(new_name, ".enc");
+    rename(file_name, new_name);
+
+    free(new_name);
+    free(key);
+}
+
+void decrypt_file(char *file_name, unsigned char *key) {
+    FILE *fptr;
+
+    AES_CTX ctx;
+    unsigned char data_buffer[AES_BLOCK_SIZE];  //  bytes storing data; 16 bytes
+
+    long long file_size = get_file_size(file_name);
+    fptr = fopen(file_name, "rb+");
+
+    AES_DecryptInit(&ctx, key);
+
+    for (long long i = 0; i < file_size; i += AES_BLOCK_SIZE) {
+        fread(data_buffer, sizeof(unsigned char), AES_BLOCK_SIZE, fptr);
+        fseek(fptr, i, SEEK_SET);
+        AES_Decrypt(&ctx, data_buffer, data_buffer);
+        fwrite(data_buffer, sizeof(unsigned char), AES_BLOCK_SIZE, fptr);
+    }
+    
+    AES_CTX_Free(&ctx);    
+    fclose(fptr);
+
+    char *new_name = (char *)calloc(255, sizeof(char));
+    strcat(new_name, file_name);
+
+    int index = strlen(new_name) - 1;
+    for (int i = index; i > index - 4; i--) {
+        new_name[i] = (char)0;
+    }
+
+    rename(file_name, new_name);
+
+    free(new_name);
+    free(key);
+}
--- src/main.c
+++ src/main.c
@@ -5,12 +5,18 @@
 #include <string.h>
 #include "../lib/AES_256_ECB.h"
 #include "../lib/SHA_256.h"
+#include "../lib/file_ops.h"
 
 void dump_bytes(unsigned char *bytes, int size) {
 	for (int i=0; i<size; i++) {
 		printf("%02x", bytes[i]);
 	}
 	printf("\n");
+}
+
+void help() {
+	printf("Usage: aes-util ARGUMENT FILE PASSWORD\n");
+	printf("ARGUMENT - encrypt, decrypt\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -29,13 +35,37 @@ int main(int argc, char *argv[]) {
 	}
 	*/
 
-	if (argc != 4) {
-		printf("Usage: aes-util ARGUMENT FILE PASSWORD\n");
-		printf("ARGUMENT - encrypt, decrypt\n");
+	if (argc != 4) {  // Handles arguments
+		help();
+		return 1;
+	}
+	if (strcmp(argv[1], "encrypt") != 0 && strcmp(argv[1], "decrypt") != 0) {
+		help();
+		printf("Wrong argument provided.\n");
+		return 1;
+	}
+	if (is_writable(argv[2]) != 0) {  // SHOULD handle relative/absolute file paths, not just current working directory...
+		help();
+		printf("Can not access file.\n");
+		return 1;
+	} // Assuming the user performs proper path handling in linux/windows on terminal.
+	
+	
+	if (hasEnc(argv[2]) != 0) {
+		printf("File does not have .enc extension and is assumed not encrypted.\n");
 		return 1;
 	}
 
-	
+
+
+	// Password check
+	if (check_password(argv[3], argv[2]) != 0) {
+		printf("Wrong password!\n");
+		printf("NOTE: Keep in mind, I assume that this file even has a password to begin with.\n");
+		return 1;
+	}
+
+
 
 	return 0;
 }
